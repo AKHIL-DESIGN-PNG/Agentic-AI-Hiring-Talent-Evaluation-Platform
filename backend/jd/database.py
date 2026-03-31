7513546cdb27b4c11190fbfe10fd48d3037f0974
AKHIL KUMAR REDDY YANAMALA <akhi4uy@gmail.com>
2026-03-31 19:47:43 +0530

    Update database.py

--- backend/jd/database.py
+++ backend/jd/database.py
@@ -8,7 +8,7 @@ load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg://postgres.xxorkvmykzufcjwlmdgj:Hackminds%4002@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require",
+    "postgresql+psycopg://postgres.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
